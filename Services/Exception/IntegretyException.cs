dbb8b156d0b3871adaebc1ee0c3f392ed86eb2bd
William Gaertner <william_nefi@hotmail.com>
2019-09-13 23:46:40 -0400

    Exception handling for delete (referential integrity)

---
+++ Services/Exception/IntegretyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services.Exception
+{
+    public class IntegretyException : ApplicationException
+    {
+        public IntegretyException(string message) : base(message)
+        {
+
+        }
+    }
+}
